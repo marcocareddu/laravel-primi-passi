898845a9982ed7433f96e16e640da698de4088ef
marcocareddu <saldumaccu@tuta.io>
2023-08-02 13:25:33 +0200

    PHP web - Added compact

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $welcome = 'Benvenuti in Laravel';
+
+    return view('home', compact('welcome'));
 });
